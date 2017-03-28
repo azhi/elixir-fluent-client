ba9380d8d6ea387400c079d1be8a8487855830e4
azhi <astzhe@gmail.com>
2017-03-28 20:31:00 +0300

    reconnect to fluent if socket is closed

--- lib/fluent/client.ex
+++ lib/fluent/client.ex
@@ -24,8 +24,12 @@ defmodule Fluent.Client do
   end
 
   def handle_cast({:send, tag, data}, state) do
-    state = send(tag, data, state)
-    {:noreply, state}
+    case send(tag, data, state) do
+      {:ok, state} ->
+        {:noreply, state}
+      {:error, reason} ->
+        {:stop, reason, state}
+    end
   end
 
   def handle_call(call, from, %State{socket: nil, options: options} = state) do
@@ -34,18 +38,32 @@ defmodule Fluent.Client do
   end
 
   def handle_call({:send, tag, data}, _from, state) do
-    state = send(tag, data, state)
-    {:reply, :ok, state}
+    case send(tag, data, state) do
+      {:ok, state} ->
+        {:reply, :ok, state}
+      {:error, reason} ->
+        {:stop, reason, :error, state}
+    end
   end
 
   defp connect(options) do
     Socket.TCP.connect!(options[:host] || "localhost",options[:port] || 24224, packet: 0)
   end
 
-  defp send(tag, data,  %State{socket: socket, serializer: serializer} = state) do
+  defp send(tag, data,  %State{socket: socket, options: options, serializer: serializer} = state, retry \\ 1) do
     packet = serializer.([tag, now, data])
-    Socket.Stream.send!(socket, packet)
-    state
+    case Socket.Stream.send(socket, packet) do
+      :ok -> {:ok, state}
+      {:error, :closed} ->
+        if retry > 5 do
+          {:error, {:socket_error, :connect_retry_exhausted}}
+        else
+          new_state = %State{state | socket: connect(options)}
+          send(tag, data, new_state, retry + 1)
+        end
+      {:error, reason} ->
+        {:error, {:socket_error, reason}}
+    end
   end
 
   defp serializer(:msgpack), do: &Msgpax.pack!/1
