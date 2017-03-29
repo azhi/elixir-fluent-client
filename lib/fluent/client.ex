392ce741a213d3869b782a579f6c2a1cbb5405fc
azhi <astzhe@gmail.com>
2017-03-29 18:48:23 +0300

    refactor connection error processing
    return to exceptions when something goes wrong, yet keep non-bang
    connect and send functions in order to process socket closing

--- lib/fluent/client.ex
+++ lib/fluent/client.ex
@@ -18,51 +18,55 @@ defmodule Fluent.Client do
     {:ok, %State{options: options, serializer: serializer}}
   end
 
-  def handle_cast(msg, %State{socket: nil, options: options} = state) do
-    socket = connect(options)
-    handle_cast(msg, %State{ state | socket: socket})
-  end
-
   def handle_cast({:send, tag, data}, state) do
-    case send(tag, data, state) do
-      {:ok, state} ->
-        {:noreply, state}
-      {:error, reason} ->
-        {:stop, reason, state}
-    end
-  end
-
-  def handle_call(call, from, %State{socket: nil, options: options} = state) do
-    socket = connect(options)
-    handle_call(call, from, %State{ state | socket: socket})
+    state = ensure_connected!(state)
+    state = send(tag, data, state)
+    {:noreply, state}
   end
 
   def handle_call({:send, tag, data}, _from, state) do
-    case send(tag, data, state) do
-      {:ok, state} ->
-        {:reply, :ok, state}
-      {:error, reason} ->
-        {:stop, reason, :error, state}
+    state = ensure_connected!(state)
+    state = send(tag, data, state)
+    {:reply, :ok, state}
+  end
+
+  defp ensure_connected!(%State{socket: nil, options: options} = state) do
+    case connect(options) do
+      {:ok, socket} ->
+        %State{state | socket: socket}
+      {:error, error} ->
+        raise RuntimeError, "Couldn't connect to fluent. Error: #{error}"
     end
   end
 
-  defp connect(options) do
-    Socket.TCP.connect!(options[:host] || "localhost",options[:port] || 24224, packet: 0)
+  defp ensure_connected!(%State{socket: socket} = state) do
+    # Nothing to do
+    state
   end
 
-  defp send(tag, data,  %State{socket: socket, options: options, serializer: serializer} = state, retry \\ 1) do
-    packet = serializer.([tag, now, data])
-    case Socket.Stream.send(socket, packet) do
-      :ok -> {:ok, state}
-      {:error, :closed} ->
+  defp connect(options, retry \\ 1) do
+    case Socket.TCP.connect(options[:host] || "localhost", options[:port] || 24224, packet: 0) do
+      {:ok, socket} -> {:ok, socket}
+      {:error, error} ->
         if retry > 5 do
-          {:error, {:socket_error, :connect_retry_exhausted}}
+          {:error, error}
         else
-          new_state = %State{state | socket: connect(options)}
-          send(tag, data, new_state, retry + 1)
+          :timer.sleep round(:math.pow(2, retry - 1) * 1000)
+          connect(options, retry + 1)
         end
-      {:error, reason} ->
-        {:error, {:socket_error, reason}}
+    end
+  end
+
+  defp send(tag, data,  %State{socket: socket, options: options, serializer: serializer} = state) do
+    packet = serializer.([tag, now(), data])
+    case Socket.Stream.send(socket, packet) do
+      :ok -> state
+      {:error, :closed} ->
+        Socket.Stream.close(socket)
+        new_state = ensure_connected!(%State{state | socket: nil})
+        send(tag, data, new_state)
+      {:error, error} ->
+        raise RuntimeError, "Error when sending data to fluent: #{error}"
     end
   end
 
